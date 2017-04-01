6acff84c8f50f98a17390036a73f23adbec64728
Vinh Tran <daivinhtran.vt@gmail.com>
2017-04-01 12:14:23 -0400

    add/edit report page

--- src/pages/new-report/new-report.ts
+++ src/pages/new-report/new-report.ts
@@ -1,22 +1,108 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NavParams, NavController } from 'ionic-angular';
+import { ModalController, LoadingController, ToastController } from 'ionic-angular';
 
-/*
-  Generated class for the NewReport page.
+import { SetLocationPage } from '../set-location/set-location';
+import { Location } from '../../models/location';
+import { Geolocation } from '@ionic-native/geolocation';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import { ReportService } from '../../providers/report';
+import { Report } from '../../models/report';
+
+
 @Component({
   selector: 'page-new-report',
   templateUrl: 'new-report.html'
 })
-export class NewReportPage {
+export class NewReportPage implements OnInit {
+  location: Location = {
+    lat: 40.7624324,
+    lng: -73.9759827
+  };
+  locationIsSet = false;
+  report: Report;
+  mode = 'Add';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    private navParams: NavParams,
+    private navCtrl: NavController,
+    private modalCtrl: ModalController,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private geolocation: Geolocation,
+    private reportService: ReportService) {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NewReportPage');
   }
 
+  ngOnInit() {
+    const mode = this.navParams.get('mode');
+    if (mode == 'Edit') {
+      this.mode = 'Edit';
+      const report = this.navParams.get('report');
+      this.report = report;
+      this.location = {
+        lat: this.report.location.lat,
+        lng: this.report.location.lng
+      };
+      this.locationIsSet = true;
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    this.reportService.addReport(
+      form.value.name,
+      form.value.condition,
+      parseInt(form.value.virusppm),
+      parseInt(form.value.contppm),
+      this.location);
+    this.location = {
+      lat: 40.7624324,
+      lng: -73.9759827
+    };
+    this.locationIsSet = false;
+    form.reset();
+    this.navCtrl.pop();
+  }
+
+  onOpenMap() {
+    const modal = this.modalCtrl.create(SetLocationPage, {
+      location: this.location, isSet: this.locationIsSet
+    });
+    modal.present();
+    modal.onDidDismiss(
+      data => {
+        if (data) {
+          console.log(this.location);
+          this.location = data.location;
+          this.locationIsSet = true;
+        }
+      });
+  }
+
+  onLocate() {
+    const loader = this.loadingCtrl.create({
+      content: 'Getting your Location...'
+    });
+    loader.present();
+    this.geolocation.getCurrentPosition()
+      .then(
+        location => {
+          loader.dismiss();
+          this.location.lat = location.coords.latitude;
+          this.location.lng = location.coords.longitude;
+          this.locationIsSet = true;
+        }
+      ).catch(
+        error => {
+          loader.dismiss();
+          console.log(error);
+          const toast = this.toastCtrl.create({
+            message: 'Could get location, please pick it manually!',
+            duration: 2500
+          });
+          toast.present();
+      })
+  }
 }
